ad69e8fed8cb868ddc5ac6c198d50bef7dd6ab5c
Nicholas Diego <nickdiegao@gmail.com>
2024-03-11 21:07:05 -0300

    Criando requisições

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, Query
+import requests
+
+app = FastAPI()
+
+@app.get('/api/hello')
+def hello_world():
+    '''
+        Endpoit hello world
+    '''
+    return {'Hello': 'World'}
+
+@app.get('/api/restaurantes/')
+def get_restaurantes(restaurante: str=Query(None)):   
+    '''
+        Endpoint para ver o cardapio dos restaurantes
+    '''
+    url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        dados_json = response.json()
+        if restaurante is None:
+            return {'Dados': dados_json}
+        
+        dados_restaurante = []
+        for item in dados_json:
+            # nome_do_restaurante = item['Company']
+            if item['Company'] == restaurante:
+                dados_restaurante.append({
+                    "item": item['Item'],
+                    "price": item['price'],
+                    "description": item['description']
+                })
+        return {'Restaurante': restaurante, 'Cardapio': dados_restaurante}
+    else:
+        return {'Erro': f'{response.status_code} - {response.text}'}
+
+
+# uvicorn main:app --reload
+# http://127.0.0.1:8000/api/hello
+# http://127.0.0.1:8000/api/restaurantes/
+# http://127.0.0.1:8000/api/restaurantes/?restaurante=McDonald’s
